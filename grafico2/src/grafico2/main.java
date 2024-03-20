a774dee4a512af1f9d53bf921bce88d4c60e331a
aluno <aluno@TG33823LAB104B.edu.ce.senac.br>
2024-03-20 16:37:41 -0300

    Tela de aviso feita em JAVA

---
+++ grafico2/src/grafico2/main.java
@@ -0,0 +1,10 @@
+package grafico2;
+import javax.swing.*;
+import java.awt.*;
+public class main {
+
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog(null,"Entre","dialogo",JOptionPane.WARNING_MESSAGE);
+				System.out.println(input) ;
+	}
+}
